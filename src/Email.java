a10302fa63165ca19620d9002d7ac0cf4a871626
SolomoonOsunde <otasowieosunde@gmail.com>
2023-06-20 03:29:04 +0100

    Initial commit

---
+++ src/Email.java
@@ -0,0 +1,23 @@
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private Integer mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor that receives the firstname and lastname
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Ask for Department
+    
+    //Generate random password
+
+    //Set the alternate email
+
+    //Change the password
+}

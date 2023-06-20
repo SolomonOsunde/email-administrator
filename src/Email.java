ddd43dc35bb7b671f3b4d0dc59045126af87a747
SolomoonOsunde <otasowieosunde@gmail.com>
2023-06-20 03:47:30 +0100

    update to app

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -11,11 +13,37 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+
+        System.out.println("Email created for " + this.firstName + " " + this.lastName);
+        //call a method asking for the department
+        this.department = setDepartment();
+
+        System.out.println("Department: " + this.department);
     }
 
     //Ask for Department
-    
+
+    private String setDepartment() {
+        System.out.println("Enter the department:\n 1 for Sales \n 2 for Accounting \n 3 for Engineering");
+        System.out.print("Enter department code : ");
+        Scanner input = new Scanner(System.in);
+        int value = input.nextInt();
+        if(value == 1){
+            return "Sales";
+        } else if (value ==2) {
+            return "Accounting";
+        } else if (value == 3) {
+            return "Engineering";
+        }else {
+            return "Others";
+        }
+    }
+
     //Generate random password
+
+    private String setPassword() {
+
+    }
 
     //Set the alternate email
 
